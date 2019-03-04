32e8099dae06af47d787e44b9920da57d165bf8c
mcridednb <na@smena.space>
2019-03-05 00:03:58 +0500

    Добавил поддержку кастомных шрифтов для линукс

--- main.cpp
+++ main.cpp
@@ -4,10 +4,24 @@
 #include "src/gatewayclient.h"
 #include <QSystemTrayIcon>
 #include <QDebug>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    // в общем, начиная с каких то недавних версий qt решил не подтягивать шрифты с собой (даже при статической сборке)
+    // ну и вынужден был заюзать данный хек
+#ifdef __linux__
+    // Load the embedded font.
+    QString fontPath = ":/bin/Ubuntu-L.ttf";
+    int fontId = QFontDatabase::addApplicationFont(fontPath);
+    if (fontId != -1)
+    {
+        QFont font("Ubuntu-L");
+        app.setFont(font);
+    }
+#endif
 
     GatewayClient *client = new GatewayClient();
     client->run();
